4eb1ec4a578e719b044cc9cd8688beeec751d67b
Stefan Pla <spla@icloud.com>
2025-01-15 11:20:30 -0500

    fixed bugs present in code - camera now follows player which moves at a reasonable speed and responds to player inputs

--- FollowPlayerX.cs
+++ FollowPlayerX.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class FollowPlayerX : MonoBehaviour
 {
-    public GameObject plane;
-    private Vector3 offset;
+    public GameObject player;
+    private Vector3 offset = new Vector3(50,0,0);
 
     // Start is called before the first frame update
     void Start()
@@ -14,8 +14,8 @@ public class FollowPlayerX : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        transform.position = plane.transform.position + offset;
+        transform.position = player.transform.position + offset;
     }
 }
